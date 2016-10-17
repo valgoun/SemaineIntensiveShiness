d934f54ac978e1c30cc22bba8ab07a89b4827df4
valgoun <valgoun@gmail.com>
2016-10-17 14:23:04 +0200

    Camera V1.0
    Camera set up :
    - distance : the distance between the target point and the camera
    - angel : angle of the camera

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform Target;
+    public float SmoothTime = 0.3f;
+    private Vector3 vel = Vector3.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LateUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, Target.position, ref vel, SmoothTime);
+    }
+}
---
+++ Assets/Scripts/Camera/CameraSetUp.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CameraSetUp : MonoBehaviour {
+
+    [Range(1f, 20f)]
+    public float Distance = 5f;
+    [Range(35f, 85f)]
+    public float Angle = 60f;
+    private Transform _target;
+
+	// Use this for initialization
+	void Start () {
+        _target = transform.parent;
+        CalcPos();
+    }
+
+    void ChangeCamera(float distance, float angle)
+    {
+
+    }
+
+    private void CalcPos()
+    {
+        Vector3 pos = transform.position;
+        pos.y = Mathf.Sin(Angle * Mathf.Deg2Rad) * Distance;
+        pos.z = Mathf.Cos(Angle * Mathf.Deg2Rad) * Distance * -1f;
+        transform.position = _target.position + pos;
+        transform.LookAt(_target);
+    }
+
+    void OnValidate()
+    {
+        if (_target == null)
+            _target = transform.parent;
+        CalcPos();
+    }
+
+}
