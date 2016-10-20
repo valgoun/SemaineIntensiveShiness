a2af3fb8e3ae234980e5d824867a8aacde0d5976
Caillou <p.chapelet@rubika-edu.com>
2016-10-20 14:37:01 +0200

    Animations finales
    Coucou Benjamin !

--- Assets/Scripts/Character/Controller/TopController.cs
+++ Assets/Scripts/Character/Controller/TopController.cs
@@ -25,10 +25,24 @@ public class TopController : Controller
 
     private bool _isBoosting = false;
 
+    public float RollingSpeed;
+    public float BlendSpeed;
+
+    private Vector3 rotation;
+    private GameObject Poui;
+    private GameObject Pivot;
+    private Animator Anim;
+    private bool _isDead;
+
     void Start()
     {
         _body = GetComponent<Rigidbody>();
         _mRend = GetComponent<MeshRenderer>();
+
+        Pivot = transform.GetChild(0).gameObject;
+        Poui = Pivot.transform.GetChild(0).gameObject;
+        rotation = new Vector3(Poui.transform.rotation.eulerAngles.x + RollingSpeed * Time.deltaTime, 0, 0);
+        Anim = Poui.GetComponent<Animator>();
     }
 
     public override void OnNoInput()
@@ -127,8 +141,26 @@ public class TopController : Controller
         {
             _body.velocity = _body.velocity.normalized * _speed;
         }
-
     }
+
+    void Update()
+    {
+        Anim.SetBool("IsRolling", _isRolling);
+        Anim.SetBool("IsDead", _isDead);
+
+        if (_isRolling)
+        {
+            Poui.transform.DOScaleY(0.7f, BlendSpeed);
+            Pivot.transform.Rotate(rotation);
+        }
+
+        if (!_isRolling)
+        {
+            Poui.transform.DOScaleY(1, BlendSpeed);
+            Pivot.transform.DORotate(new Vector3(0, 90, 0), 0.1f);
+        }
+    }
+
     void OnCollisionExit(Collision other)
     {
         if (other.collider.transform.CompareTag("Wall"))
